ee8f6113ec4f6554b7c64fc6077f6b21ce959ac4
becklau <146844332+becklau@users.noreply.github.com>
2024-05-16 11:22:40 +0100

    Add files via upload

---
+++ js/chall2.js
@@ -0,0 +1,14 @@
+function billingCheck(){
+    console.log("Made it")
+    var billing = document.querySelector("#bill").value;
+    var is_checked = document.querySelector("#sameAddress").checked
+    console.log(is_checked)
+    if (is_checked){
+        document.querySelector("#home").value = billing
+        document.querySelector("#home").disabled = true
+    } else {
+        document.querySelector("#home").disabled = false
+    }
+    var home = document.querySelector("#home")
+
+}
